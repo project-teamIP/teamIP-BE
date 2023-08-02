b1ec829857ab8e80eeffc54eac305b7a2bf17406
hyun1024 <haema3043@gmail.com>
2023-08-02 20:51:39 +0900

    Chore : yml파일 교체

---
+++ src/main/java/com/teamip/heyhello/global/auth/JwtAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.teamip.heyhello.global.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teamip.heyhello.domain.user.dto.LoginRequestDto;
+import com.teamip.heyhello.domain.user.dto.StatusResponseDto;
+import com.teamip.heyhello.global.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final JwtUtil jwtUtil;
+    private final ObjectMapper objectMapper;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, ObjectMapper objectMapper) {
+        this.jwtUtil = jwtUtil;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            LoginRequestDto loginRequestDto = objectMapper.readValue(request.getInputStream(), LoginRequestDto.class);
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(), loginRequestDto.getPassword());
+            return getAuthenticationManager().authenticate(authenticationToken);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String token = jwtUtil.createToken(((UserDetails) authResult.getPrincipal()).getUsername());
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(HttpStatus.OK.value());
+        StatusResponseDto responseDto = new StatusResponseDto(HttpStatus.OK, "로그인 성공");
+        String responseString = objectMapper.writeValueAsString(responseDto);
+        response.getWriter().write(responseString);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        log.info(failed.getMessage());
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        StatusResponseDto responseDto = new StatusResponseDto(HttpStatus.UNAUTHORIZED, failed.getMessage());
+        String responseString = objectMapper.writeValueAsString(responseDto);
+        response.getWriter().write(responseString);
+    }
+}
---
+++ src/main/java/com/teamip/heyhello/global/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.teamip.heyhello.global.auth;
+
+import com.teamip.heyhello.domain.user.entity.User;
+import com.teamip.heyhello.domain.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("존재하지 않는 이메일")
+        );
+
+        return new UserDetailsImpl(user);
+    }
+}
