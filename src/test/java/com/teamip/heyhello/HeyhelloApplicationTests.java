13b45382b1662790e108d2c55c27f68687d6ab73
hyun1024 <haema3043@gmail.com>
2023-08-02 09:51:40 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/teamip/heyhello/HeyhelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamip.heyhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyhelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
