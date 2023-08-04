2422fbad65f7c3a6dcf333aaf8d2c11492018b1d
userkim12 <potomq444@naver.com>
2023-08-04 21:06:32 +0900

    [#23] Feat: Helper 클래스 작성

---
+++ src/main/java/com/teamip/heyhello/domain/user/helper/UserFinder.java
@@ -0,0 +1,28 @@
+package com.teamip.heyhello.domain.user.helper;
+
+import com.teamip.heyhello.domain.user.entity.User;
+import com.teamip.heyhello.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class UserFinder {
+    private final UserRepository userRepository;
+
+    public User findUserByLoginId(String loginId) {
+        Optional<User> user = userRepository.findByLoginId(loginId); 
+        return user.orElseThrow(
+                () -> new RuntimeException("존재하지 않는 사용자입니다.") 
+        );
+    }
+
+    public User findUserByNickname(String nickname) {
+        Optional<User> user = userRepository.findByNickname(nickname);
+        return user.orElseThrow(
+                () -> new RuntimeException("존재하지 않는 사용자입니다.")
+        );
+    }
+}
