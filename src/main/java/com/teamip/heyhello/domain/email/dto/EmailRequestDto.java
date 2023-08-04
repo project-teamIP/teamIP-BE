2d91d11918023583547a3dd367b6f33d2fb6cbd4
userkim12 <potomq444@naver.com>
2023-08-04 09:51:12 +0900

    [#7] Feat: 이메일 전송 기능 추가

---
+++ src/main/java/com/teamip/heyhello/domain/email/dto/EmailRequestDto.java
@@ -0,0 +1,10 @@
+package com.teamip.heyhello.domain.email.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EmailRequestDto {
+    private String address;
+}
