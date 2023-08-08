502a4cc5c22e2ad0bad850b04482df129bcce850
userkim12 <potomq444@naver.com>
2023-08-08 16:48:12 +0900

    [#40] Feat: 더미데이터 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (is_blocked, is_google, is_kakao, clean_point,
+                   language, country, login_id, nickname, password,
+                   interest, gender)
+VALUES (false, false, false, 50, 'KOREAN', 'SOUTH_KOREA', 'test01@gmail.com', '유저01', '$2a$10$nRiw2L4edlhC2.kSuy70XONhXgpVeWgXRAfsPp.NiFsBXMxhoOYr2', 'GAME', 'MALE'),
+       (false, false, false, 50, 'ENGLISH', 'US', 'test02@gmail.com', 'user02', '$2a$10$t.gFil4BXaDsbChhewyUvujK2SuT3I1bIcZIc4B6/1l7MY2X5b3Z.', 'K-POP', 'MALE'),
+       (false, false, false, 50, 'KOREAN', 'SOUTH_KOREA', 'test03@gmail.com', '유저03', '$2a$10$nRiw2L4edlhC2.kSuy70XONhXgpVeWgXRAfsPp.NiFsBXMxhoOYr2', 'GAME', 'FEMALE'),
+       (false, false, false, 50, 'ENGLISH', 'UK', 'test04@gmail.com', 'user04', '$2a$10$t.gFil4BXaDsbChhewyUvujK2SuT3I1bIcZIc4B6/1l7MY2X5b3Z.', 'K-POP', 'FEMALE'),
+       (false, false, false, 50, 'ENGLISH', 'JAPAN', 'test05@gmail.com', 'user05', '$2a$10$t.gFil4BXaDsbChhewyUvujK2SuT3I1bIcZIc4B6/1l7MY2X5b3Z.', 'MOVIE', 'FEMALE'),
+       (false, false, false, 50, 'KOREAN', 'US', 'test06@gmail.com', '유저06', '$2a$10$t.gFil4BXaDsbChhewyUvujK2SuT3I1bIcZIc4B6/1l7MY2X5b3Z.', 'BOOK', 'MALE'),
+       (false, false, false, 50, 'KOREAN', 'US', 'test07@gmail.com', '유저07', '$2a$10$t.gFil4BXaDsbChhewyUvujK2SuT3I1bIcZIc4B6/1l7MY2X5b3Z.', 'BOOK', 'FEMALE');
+
+
