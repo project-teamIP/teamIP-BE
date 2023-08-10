2f2160ac1a35f5f77eed72e9e38ed2e2a3b06f2c
userkim12 <potomq444@naver.com>
2023-08-10 16:37:39 +0900

    [#53] Test: ReportService 단위 테스트 작성

--- src/main/java/com/teamip/heyhello/domain/user/dto/SignupRequestDto.java
+++ src/main/java/com/teamip/heyhello/domain/user/dto/SignupRequestDto.java
@@ -10,4 +10,10 @@ public class SignupRequestDto {
     private String loginId;
     private String password;
     private String nickname;
+
+    public SignupRequestDto(String loginId, String password, String nickname) {
+        this.loginId = loginId;
+        this.password = password;
+        this.nickname = nickname;
+    }
 }
