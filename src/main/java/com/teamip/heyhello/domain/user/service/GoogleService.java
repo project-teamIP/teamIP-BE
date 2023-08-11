ee1bb86d94724bb8af3f589b798c97a18b9ea031
jonghyeon1013 <dkswhdgus147@naver.com>
2023-08-11 21:16:20 +0900

    [#17] Feat: 소셜 로그인 기능 구현 - 구글

---
+++ src/main/java/com/teamip/heyhello/domain/user/service/GoogleService.java
@@ -0,0 +1,141 @@
+package com.teamip.heyhello.domain.user.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teamip.heyhello.domain.user.dto.GoogleUserInfoDto;
+import com.teamip.heyhello.domain.user.entity.User;
+import com.teamip.heyhello.domain.user.repository.UserRepository;
+import com.teamip.heyhello.global.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.UUID;
+
+@Slf4j(topic = "GOOGLE Login")
+@Service
+@RequiredArgsConstructor
+public class GoogleService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final RestTemplate restTemplate;
+    private final JwtUtil jwtUtil;
+
+    public String googleLogin(String code) throws JsonProcessingException {
+        // 1. "인가 코드"로 "액세스 토큰" 요청
+        String accessToken = getToken(code);
+
+        // 2. 토큰으로 구글 API 호출 : "액세스 토큰"으로 "구글 사용자 정보" 가져오기
+        GoogleUserInfoDto googleUserInfo = getGoogleUserInfo(accessToken);
+
+        // 3. 필요시에 회원가입
+        User googleUser = registerGoogleUserIfNeeded(googleUserInfo);
+
+        // 4. JWT 토큰 반환
+        String createToken = jwtUtil.createToken(googleUser.getLoginId());
+
+        return createToken;
+    }
+
+    private String getToken(String code) throws JsonProcessingException {
+        log.info("인가코드 : " + code);
+        // 요청 URL 만들기
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://www.googleapis.com")
+                .path("/oauth2/v4/token")
+                .encode()
+                .build()
+                .toUri();
+
+        // HTTP Header 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HTTP Body 생성
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", "366803150284-5hqk1vqq15s6aqjfa3d9om2j1t9c2r0u.apps.googleusercontent.com");
+        body.add("client_secret", "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");
+        body.add("redirect_uri", "http://localhost:8080/api/users/login/google");
+        body.add("code", code);
+
+        RequestEntity<MultiValueMap<String, String>> requestEntity = RequestEntity
+                .post(uri)
+                .headers(headers)
+                .body(body);
+
+        // HTTP 요청 보내기
+        ResponseEntity<String> response = restTemplate.exchange(
+                requestEntity,
+                String.class
+        );
+
+        // HTTP 응답 (JSON) -> 액세스 토큰 파싱
+        JsonNode jsonNode = new ObjectMapper().readTree(response.getBody());
+        return jsonNode.get("access_token").asText();
+    }
+    private GoogleUserInfoDto getGoogleUserInfo(String accessToken) throws JsonProcessingException {
+        log.info("accessToken : " + accessToken);
+        // 요청 URL 만들기
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://www.googleapis.com")
+                .path("/oauth2/v3/userinfo")
+                .encode()
+                .build()
+                .toUri();
+
+        // HTTP Header 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessToken);  //토큰이란것을 알려주는 식별자 붙이기
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        RequestEntity<MultiValueMap<String, String>> requestEntity = RequestEntity
+                .post(uri)
+                .headers(headers)
+                .body(new LinkedMultiValueMap<>());
+
+        // HTTP 요청 보내기
+        ResponseEntity<String> response = restTemplate.exchange(
+                requestEntity,
+                String.class
+        );
+
+        JsonNode jsonNode = new ObjectMapper().readTree(response.getBody());
+        String id = jsonNode.get("sub").toString();
+        log.info("구글 사용자 정보: " + id);
+        return new GoogleUserInfoDto(id);
+    }
+
+    private User registerGoogleUserIfNeeded(GoogleUserInfoDto googleUserInfo) {
+        String loginId = googleUserInfo.getId();
+        User googleUser = userRepository.findByLoginId(loginId).orElse(null);
+
+        if (googleUser == null) {
+            String googleId = googleUserInfo.getId();
+            String password = UUID.randomUUID().toString();
+            String encodedPassword = passwordEncoder.encode(password);
+            String nickname = UUID.randomUUID().toString();
+
+            googleUser = User.builder()
+                    .loginId(googleId)
+                    .password(encodedPassword)
+                    .nickname(nickname)
+                    .build();
+
+            userRepository.save(googleUser);
+        }
+        return googleUser;
+    }
+
+}
