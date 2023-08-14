6fbd31fa671eb77071a20a3b4537b88a297b7ddb
userkim12 <potomq444@naver.com>
2023-08-14 23:16:04 +0900

    Fix: github actions 적용을 위한 몸부림

--- src/main/java/com/teamip/heyhello/global/config/JasyptConfig.java
+++ src/main/java/com/teamip/heyhello/global/config/JasyptConfig.java
@@ -3,17 +3,20 @@ package com.teamip.heyhello.global.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JasyptConfig {
+    @Value("${jasypt.encryptor.password}")
+    private String key;
 
     @Bean
     public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(System.getenv("JASYPT_ENCRYPTOR_KEY"));
+        config.setPassword(key);
         config.setPoolSize("1");
         config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
         config.setStringOutputType("base64");
