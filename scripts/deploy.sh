41196b5817d8c44160157e015a9bd85d259c9e5b
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-08-16 01:12:45 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/heyhi
-
-cd $REPOSITORY
+REPOSITORY="/home/ubuntu/heyhi"
+TIME_NOW=$(date +%c)
 
 APP_NAME=heyhi-project
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
@@ -13,12 +12,12 @@ DEPLOY_LOG="$REPOSITORY/deploy.log"
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 실행 중인 애플리케이션이 없습니다. <" >> $DEPLOY_LOG
+  echo "$TIME_NOW > 실행 중인 애플리케이션이 없습니다. <" >> $DEPLOY_LOG
 else
-  echo "> 실행 중인 $CURRENT_PID 종료합니다. <" >> $DEPLOY_LOG
+  echo "$TIME_NOW > 실행 중인 $CURRENT_PID 종료합니다. <" >> $DEPLOY_LOG
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포"
+echo "$TIME_NOW > $JAR_PATH 배포" >> $DEPLOY_LOG
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
