97b501da07ebb48daa8b1decc92af9fb47923fa1
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-08-16 14:45:24 +0900

    function 수정
    현재 실행 중인 PID 까지만 출력되고 CURRENT_PID는 출력이 안돼서 배포가 실패하는 것 같아서 수정해봄

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-function get_pid() {
-  pgrep -f $APP_NAME
-}
-
 REPOSITORY="/home/ubuntu/heyhi"
 TIME_NOW=$(date +%c)
 
 APP_NAME=heyhello
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep SNAPSHOT.jar | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-CURRENT_PID=$(get_pid)
 
 DEPLOY_LOG="$REPOSITORY/deploy.log"
+
+function get_pid() {
+  $(pgrep -f $APP_NAME)
+}
+
+CURRENT_PID=$(get_pid)
 
 echo "[$TIME_NOW] > 현재 실행 중인 .jar PID: $CURRENT_PID" >> $DEPLOY_LOG
 
