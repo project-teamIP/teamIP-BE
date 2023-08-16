76b54913ac9b2daa5bdc5fcef938300da3c21493
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-08-16 16:29:25 +0900

    CURRENT_PID 따옴표 제거

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,15 +7,15 @@ JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep SNAPSHOT.jar | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 DEPLOY_LOG="$REPOSITORY/deploy.log"
 
-CURRENT_PID=$(pgrep -f "$APP_NAME")
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
-echo "[$TIME_NOW] > 현재 실행 중인 .jar PID: $CURRENT_PID" >> $DEPLOY_LOG
+echo "[$TIME_NOW] > 현재 실행 중인 .jar PID=$CURRENT_PID" >> $DEPLOY_LOG
 
 if [ -z $CURRENT_PID ]
 then
   echo "[$TIME_NOW] > 실행 중인 애플리케이션이 없습니다." >> $DEPLOY_LOG
 else
-  echo "[$TIME_NOW] > PID: $CURRENT_PID 종료." >> $DEPLOY_LOG
+  echo "[$TIME_NOW] > PID=$CURRENT_PID 종료." >> $DEPLOY_LOG
   kill -15 $CURRENT_PID
   sleep 10
   if ps -f $CURRENT_PID > /dev/null
@@ -27,5 +27,5 @@ fi
 echo "[$TIME_NOW] > $JAR_PATH 배포" >> $DEPLOY_LOG
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
 
-CURRENT_PID=$(pgrep -f "$APP_NAME")
-echo "[$TIME_NOW] > 재배포된 .jar PID: $CURRENT_PID" >> $DEPLOY_LOG
+CURRENT_PID=$(pgrep -f $APP_NAME)
+echo "[$TIME_NOW] > 재배포된 .jar PID=$CURRENT_PID" >> $DEPLOY_LOG
