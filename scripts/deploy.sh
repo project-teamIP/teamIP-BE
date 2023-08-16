33029bec88f734aff55f0c10981ea96d568baead
userkim12 <69836858+userkim12@users.noreply.github.com>
2023-08-16 16:20:52 +0900

    function 제거

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,20 +2,13 @@
 
 REPOSITORY="/home/ubuntu/heyhi"
 TIME_NOW=$(date +%c)
-
-APP_NAME=heyhello
+APP_NAME='heyhello'
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep SNAPSHOT.jar | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-
 DEPLOY_LOG="$REPOSITORY/deploy.log"
 
-function get_pid() {
-  ps aux | grep java | grep $APP_NAME | grep -v grep | awk '{print $2}'
-}
+CURRENT_PID=$(pgrep -f "$APP_NAME")
 
-CURRENT_PID=$(get_pid)
-
-echo "왜 PID 출력 안됨????? 출력해봐! [$CURRENT_PID]"
 echo "[$TIME_NOW] > 현재 실행 중인 .jar PID: $CURRENT_PID" >> $DEPLOY_LOG
 
 if [ -z $CURRENT_PID ]
@@ -34,5 +27,5 @@ fi
 echo "[$TIME_NOW] > $JAR_PATH 배포" >> $DEPLOY_LOG
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
 
-CURRENT_PID=$(get_pid)
+CURRENT_PID=$(pgrep -f "$APP_NAME")
 echo "[$TIME_NOW] > 재배포된 .jar PID: $CURRENT_PID" >> $DEPLOY_LOG
