77d4fe70b40c83de45eeb687dac348929642eead
hyun1024 <haema3043@gmail.com>
2023-08-24 00:23:16 +0900

    [#99] Feat: userController swagger적용

--- src/main/java/com/teamip/heyhello/domain/user/controller/UserController.java
+++ src/main/java/com/teamip/heyhello/domain/user/controller/UserController.java
@@ -34,13 +34,13 @@ public class UserController implements TestController {
     private final GoogleService googleService;
     private final BuddyService buddyService;
     private final JwtUtil jwtUtil;
-
+    @Override
     @PostMapping("/signup")
     public ResponseEntity<StatusResponseDto> signup(@RequestBody SignupRequestDto signupRequestDto) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.signup(signupRequestDto));
     }
-
+    @Override
     @DeleteMapping("/withdrawal")
     public ResponseEntity<StatusResponseDto> withdrawal(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                         @RequestHeader("AccessToken") String atk) {
@@ -48,13 +48,13 @@ public class UserController implements TestController {
 
         return ResponseEntity.status(response.getStatus()).body(response);
     }
-
+    @Override
     @GetMapping("/mypage")
     public ResponseEntity<MypageResponseDto> getMypage(@AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         return ResponseEntity.ok(userService.getMypage(userDetails));
     }
-
+    @Override
     @PatchMapping
     public ResponseEntity<StatusResponseDto> updateProfile(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                            @RequestBody UpdateProfileDto updateProfileDto) {
@@ -62,7 +62,7 @@ public class UserController implements TestController {
         return ResponseEntity.ok(userService.updateProfile(userDetails, updateProfileDto));
     }
 
-
+    @Override
     @GetMapping("/check")
     public ResponseEntity<StatusResponseDto> checkDuplicated(@RequestParam(required = false) String email,
                                                              @RequestParam(required = false) String nickname) {
@@ -70,21 +70,21 @@ public class UserController implements TestController {
         return ResponseEntity.ok().body(userService.checkDuplicated(email, nickname));
     }
 
-
+    @Override
     @PostMapping("/buddy/{nickname}")
     public ResponseEntity<StatusResponseDto> addBuddy(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                       @PathVariable String nickname) {
 
         return ResponseEntity.ok(buddyService.addBuddy(userDetails, nickname));
     }
-
+    @Override
     @GetMapping("/buddy")
     public ResponseEntity<Page<BuddyResponseDto>> getBuddies(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                              @PageableDefault(page = 1, size = 5, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
 
         return ResponseEntity.ok(buddyService.getBuddies(userDetails, pageable));
     }
-
+    @Override
     @DeleteMapping("/buddy/{nickname}")
     public ResponseEntity<StatusResponseDto> deleteBuddy(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                          @PathVariable String nickname) {
@@ -92,7 +92,7 @@ public class UserController implements TestController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT)
                 .body(buddyService.deleteBuddy(userDetails, nickname));
     }
-
+    @Override
     @PutMapping("/image")
     public ResponseEntity<UrlResponseDto> modifyProfileImage(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
@@ -100,24 +100,25 @@ public class UserController implements TestController {
 
         return ResponseEntity.ok(userService.modifyProfileImage(userDetails, image));
     }
-
+    @Override
     @GetMapping("/login/kakao")
     public ResponseEntity<LoginResponseDto> kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
 
         return kakaoService.kakaoLogin(code);
     }
-
+    @Override
     @GetMapping("/login/google")
     public ResponseEntity<LoginResponseDto> googleLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
 
         return googleService.googleLogin(code);
     }
-
+    @Override
     @GetMapping("/auth/social")
-    public ResponseEntity<StatusResponseDto> getKakaoTokenForWithdrawal(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException{
+    public ResponseEntity<StatusResponseDto> getKakaoTokenForWithdrawal(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
 
         return kakaoService.getKakaoTokenForWithdrawal(code);
     }
+    @Override
     @PostMapping("/withdrawal/social")
     public ResponseEntity<StatusResponseDto> withdrawal(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                         @RequestParam String provider,
@@ -133,24 +134,24 @@ public class UserController implements TestController {
             throw new IllegalArgumentException("유효한 provider가 아닙니다.");
         }
     }
-
+    @Override
     @GetMapping("/count")
     public ResponseEntity<ActiveUserResponseDto> countActiveUser(){
         return ResponseEntity.ok().body(new ActiveUserResponseDto(userService.countActiveUser()));
     }
-
+    @Override
     @PutMapping("/point")
     public ResponseEntity<StatusResponseDto> rateCleanPoint(@RequestHeader("AccessToken") String tokenValue,
                                                             @RequestBody CleanPointRequestDto requestDto){
         User user = jwtUtil.getUserFromToken(tokenValue);
         return ResponseEntity.ok().body(userService.rateCleanPoint(user, requestDto.getPartnerNickname(), requestDto.getPoint()));
     }
-
+    @Override
     @GetMapping("/dashboard")
     public ResponseEntity<DashBoardResponseDto> getDashBoardInfo(@AuthenticationPrincipal UserDetailsImpl userDetails){
         return ResponseEntity.ok(userService.getDashBoardInfo(userDetails));
     }
-
+    @Override
     @GetMapping("/test")
     public ResponseEntity<DashBoardResponseDto> getDashBoard(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return ResponseEntity.ok(userService.getDashBordData(userDetails));
